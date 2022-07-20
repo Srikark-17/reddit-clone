561eead4d557976ddbe7eb6bd2c9e5a398b813a8
Srikark-17 <srikar.sgk@gmail.com>
2022-07-20 13:21:00 -0500

    Built the frontend for the postbox component and added logic for error cases

---
+++ components/Postbox.tsx
@@ -0,0 +1,116 @@
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+import Avatar from "./Avatar";
+import { LinkIcon, PhotographIcon } from "@heroicons/react/outline";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  postTitle: string;
+  postBody: string;
+  postImage: string;
+  subreddit: string;
+};
+
+const Postbox = () => {
+  const { data: session } = useSession();
+  const [imageBoxOpen, setImageBoxOpen] = useState(false);
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = handleSubmit(async (formData) => {
+    console.log(formData);
+  });
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="sticky top-16 z-50 border-rounded bg-white rounded-md border-gray-300 p-2"
+    >
+      <div className="flex items-center space-x-3">
+        <Avatar />
+
+        <input
+          {...register("postTitle", { required: true })}
+          disabled={!session}
+          className="rounded-md flex-1 bg-gray-50 p-2 pl-5 outline-none"
+          type="text"
+          placeholder={
+            session ? "Enter the title for your post" : "Sign in to post"
+          }
+        />
+
+        <PhotographIcon
+          onClick={() => setImageBoxOpen(!imageBoxOpen)}
+          className={`h-6 text-gray-100 cursor-pointer ${
+            imageBoxOpen && "text-blue-300"
+          }`}
+        />
+        <LinkIcon className="h-6 text-gray-300" />
+      </div>
+
+      {!!watch("postTitle") && (
+        <div className="flex flex-col py-2">
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("postBody")}
+              type="text"
+              placeholder="Text (optional)"
+            />
+          </div>
+
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Subreddit:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("subreddit", { required: true })}
+              type="text"
+              placeholder="i.e. react"
+            />
+          </div>
+
+          {imageBoxOpen && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Image URL:</p>
+              <input
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                {...register("postImage")}
+                type="text"
+                placeholder="Type the link of your image here (optional)"
+              />
+            </div>
+          )}
+
+          {Object.keys(errors).length > 0 && (
+            <div className="space-y-2 p-2 text-red-500">
+              {errors.postTitle?.type === "required" && (
+                <p>- A post title is required</p>
+              )}
+
+              {errors.subreddit?.type === "required" && (
+                <p>- A Subreddit is required</p>
+              )}
+            </div>
+          )}
+          {!!watch("postTitle") && (
+            <button
+              type="submit"
+              className="w-full rounded-full bg-blue-400 p-2 text-white"
+            >
+              Create Post
+            </button>
+          )}
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default Postbox;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Postbox from "../components/Postbox";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="max-w-5xl mx-auto my-7">
       <Head>
         <title>Reddit Clone</title>
       </Head>
+      <div>
+        <Postbox />
+      </div>
+      <div className="flex"></div>
     </div>
   );
 };
